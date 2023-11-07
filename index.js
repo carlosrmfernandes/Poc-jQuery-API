2adc3d8a109206109e70b826765934f850ab3038
carlospicpay <carlos.fernandes@picpay.com>
2023-11-06 21:39:43 -0300

    enviando o projeto

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 $(document).ready(function(){
-    $("#cep").click(function(){
-        var settings = {
-            "url": "https://viacep.com.br/ws/01001000/json/",
-            "method": "GET",            
-          };
-          
-          $.ajax(settings).done(function (response) {
-            alert(JSON.stringify(response));
-          });
-    });
-  });
+  $("#buscar").click(function(){        
+    var settings = {
+        "url": '',
+        "method": "GET",            
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        
+      });
+});
 
-  $(document).ready(function(){
-    $("#pagando-boleto").click(function(){
-        var settings = {
-            "url": "https://api-go-wash-efc9c9582687.herokuapp.com/api/pay-boleto",
-            "method": "POST",            
-            "headers": {
-              "Authorization": "Vf9WSyYqnwxXODjiExToZCT9ByWb3FVsjr",
-              "Content-Type": "application/json",              
-            },
-            "data": JSON.stringify({
-              "boleto": "12345678",
-              "user_id": 2
-            }),
-          };
-          
-          $.ajax(settings).done(function (response) {
-            alert(JSON.stringify(response));
-          });    
-    });
-  });
+});
+
