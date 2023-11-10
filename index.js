211f15c991a0d3de83defa070177ff9531ac31ef
carlospicpay <carlos.fernandes@picpay.com>
2023-11-09 21:34:07 -0300

    seding the project

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
+  
   $("#btn-cep").click(function(){
-    let cep = $('#cep').val();
+
+   let cep = $('#cep').val();
 
     var settings = {
         "url": `https://viacep.com.br/ws/${cep}/json/`,
         "method": "GET",            
       };
       
-      $.ajax(settings).done(function (response) {   
+      $.ajax(settings).done(function (response) {  
+        $('.uf').val(response.uf);
+        $('#logradouro').val(response.logradouro);
+        $('#bairro').val(response.bairro);
+        
       });
-});
+  });
 
 });
